41904b01a3179cfc9726601a453365d90044b3c7
Kumar.Kartik <Kumar.Kartik@LDIGITBGL-L0252.godigit.corp>
2023-07-21 15:42:00 +0530

    initial commit

--- src/main/java/com/digit/project/v2/Admin.java
+++ src/main/java/com/digit/project/v2/Admin.java
@@ -4,5 +4,28 @@ import java.util.Scanner;
 
 public class Admin {
 	Scanner sc = new Scanner(System.in);
+	String userNameO;
+	String admnPasswordO;
 	
+	
+	String userNameI;
+	String admnPasswordI;
+	
+	public Admin()
+	{
+		String userNameO="Admin";
+		String admnPasswordO="Admin";
+	}
+	
+	void AdminLogin()
+	{
+		System.out.println("Enter Username:");
+		userNameI = sc.next();
+		System.out.println("Enter Password:");
+		admnPasswordI = sc.next();
+	}
+	void RegisterCourse()
+	{
+		System.out.println("Enter The no. of Courses you need to Register");
+	}
 }
