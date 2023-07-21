3a1dd0387b1e5cb29f2bc848ee446e62d36d59c4
Logesh.R <Logesh.R@DIGTBGL-L6988.godigit.corp>
2023-07-21 16:52:39 +0530

    Initial commit

--- src/main/java/com/digit/project/v2/Professor.java
+++ src/main/java/com/digit/project/v2/Professor.java
@@ -1,5 +1,34 @@
 package com.digit.project.v2;
-
+import java.util.*;
 public class Professor {
-
+	public static void main(String[] args) {
+		Admin admin = new Admin();
+		Scanner sc = new Scanner(System.in);
+		String usernameO = "Professor";
+		String passwordO = "professor";
+		System.out.println("Enter the Username : ");
+		String username = sc.nextLine();
+		System.out.println("Enter the Password : ");
+		String password = sc.nextLine();
+		if(username.equals(usernameO) && password.equals(passwordO)) {
+			System.out.println("Login Successful");
+			System.out.println();
+			System.out.println("1. Grade Students");
+			System.out.println("2. View Student Details");
+			int input = sc.nextInt();
+			switch(input) {
+			case 1 :
+				
+			}
+		}
+		else {
+			System.out.println("Invalid Credentials");
+		}
+	}
+	
+	public void StudentDetails() {
+		
+	}
 }
+
+
