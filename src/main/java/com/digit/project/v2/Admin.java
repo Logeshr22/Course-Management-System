178ef0b2f979635343f3362b781147c0c6b70c70
Kumar.Kartik <Kumar.Kartik@LDIGITBGL-L0252.godigit.corp>
2023-07-22 13:35:02 +0530

    final commit of admin

--- src/main/java/com/digit/project/v2/Admin.java
+++ src/main/java/com/digit/project/v2/Admin.java
@@ -4,21 +4,24 @@ import java.util.ArrayList;
 
 import java.util.Scanner;
 
-
 class AdminComponent{
     Scanner sc = new Scanner(System.in);
+    
     String userNameO;
     String admnPasswordO;
     boolean Login =false;
     String userNameI;
     String admnPasswordI;
     
+    ArrayList<Integer> crs_id = new ArrayList<Integer>();
+    ArrayList<String> crs_name = new ArrayList<String>();
+    
     ArrayList<Integer> stud_id = new ArrayList<Integer>();
     ArrayList<String> stud_name = new ArrayList<String>();
     
     ArrayList<Integer> prof_id = new ArrayList<Integer>();
     ArrayList<String> prof_name = new ArrayList<String>();
-
+    
     AdminComponent()
 
     {
@@ -54,7 +57,6 @@ class AdminComponent{
         }
     }
     
-
     void createStudent(int member_cnt)
     {
      
@@ -65,6 +67,7 @@ class AdminComponent{
             System.out.println("Enter Student Name:");
             stud_name.add(sc.next());
         }
+        System.out.println("Student id and Name");
         System.out.println(stud_id);
         System.out.println(stud_name);
     }
@@ -78,10 +81,24 @@ class AdminComponent{
             System.out.println("Enter Professor Name:");
             prof_name.add(sc.next());
         }
+        System.out.println("Professor id and Name");
         System.out.println(prof_id);
         System.out.println(prof_name);    
     }
-
+    
+    void CreateCourse()
+    {
+    	crs_id.add(0);
+		crs_name.add("java");
+		crs_id.add(1);
+		crs_name.add("Sql");
+		crs_id.add(2);
+		crs_name.add("C++");
+		crs_id.add(3);
+		crs_name.add("Python");
+		crs_id.add(4);
+		crs_name.add("Devops");
+    }
 }
 
 public class Admin {
@@ -89,11 +106,9 @@ public class Admin {
     	Scanner sc = new Scanner(System.in);
         AdminComponent ad = new AdminComponent();
         ad.AdminLogin();
-        
+        //addition of professor and student 
         System.out.println("Enter the no. of Proffessor and Students to be inserted");
         int member_cnt = sc.nextInt();
-        
-        
         ad.createProffesor(member_cnt);
         ad.createStudent(member_cnt);
         
@@ -101,13 +116,14 @@ public class Admin {
         
         Professor pf = new Professor();
         pf.ProfessorDetails(ad.prof_id, ad.prof_name);
+        
         Student st = new Student();
         st.StudentDetails(ad.stud_id, ad.stud_name);
         
-        
-        Course crs = new Course();
-        
-        
+        System.out.println("couse id and Name");
+        ad.CreateCourse();
+        System.out.println(ad.crs_id);
+        System.out.println(ad.crs_name);
     }
     
 }
--- src/main/java/com/digit/project/v2/Student.java
+++ src/main/java/com/digit/project/v2/Student.java
@@ -1,11 +1,6 @@
 package com.digit.project.v2;
 import java.util.*;
 public class Student {
-	public static void main(String[] args) {
-		
-		
-	}
-	
 	public void StudentDetails(ArrayList<Integer> id, ArrayList<String> name) {
 		System.out.println(id);
 		System.out.println(name);
