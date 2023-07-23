4c9fc21023a1b92237c972cb2b4db5bd9dcfdc64
Logeshr22 <logesh.r22@gmail.com>
2023-07-23 08:26:27 +0530

    Modified Admin Module.

--- src/main/java/com/digit/project/v2/Admin.java
+++ src/main/java/com/digit/project/v2/Admin.java
@@ -1,27 +1,26 @@
-
 package com.digit.project.v2;
 import java.util.ArrayList;
 
-import java.util.Scanner;
+import java.util.*;
+
 
 class AdminComponent{
     Scanner sc = new Scanner(System.in);
-    
     String userNameO;
     String admnPasswordO;
     boolean Login =false;
     String userNameI;
     String admnPasswordI;
-    
-    ArrayList<Integer> crs_id = new ArrayList<Integer>();
-    ArrayList<String> crs_name = new ArrayList<String>();
-    
+   
     ArrayList<Integer> stud_id = new ArrayList<Integer>();
     ArrayList<String> stud_name = new ArrayList<String>();
-    
+   
     ArrayList<Integer> prof_id = new ArrayList<Integer>();
     ArrayList<String> prof_name = new ArrayList<String>();
     
+    ArrayList<Integer> crs_id = new ArrayList<Integer>();
+    ArrayList<String> crs_name = new ArrayList<String>();
+
     AdminComponent()
 
     {
@@ -29,101 +28,99 @@ class AdminComponent{
         admnPasswordO="Admin";
     }
 
-    void LoginChk(String userNameI, String admnPasswordI)
+    boolean LoginChk(String userNameI, String admnPasswordI)
     {
-        if(userNameO.equals(userNameI))
-        {
-            if(admnPasswordI.equals(admnPasswordO))
-            {
-                Login=true;
-            }
-        }
+        if(userNameO.equals(userNameI) && admnPasswordI.equals(admnPasswordO))
+        	return true;
+        else
+        	return false;
     }
 
-    void AdminLogin()
+    boolean AdminLogin()
     {
+    	System.out.println("==== WELCOME ADMIN ====");
         System.out.println("Enter Username:");
         userNameI = sc.next();
         System.out.println("Enter Password:");
         admnPasswordI = sc.next();
         LoginChk(userNameI, admnPasswordI);
-        if(Login==true)
+        if(LoginChk(userNameI,admnPasswordI)==true)
         {
             System.out.println("Login successful");
+            return true;
         }
-        if(Login==false)
+        else
         {
             System.out.println("Login Failed");
+            return false;
         }
     }
-    
+   
+
     void createStudent(int member_cnt)
     {
      
-        
+       
         for(int i=0;i<member_cnt;i++)
         {
-            stud_id.add(i);
-            System.out.println("Enter Student Name:");
+            System.out.println("Enter name of Student "+(i+1)+" :");
             stud_name.add(sc.next());
+            stud_id.add(i+1);
         }
-        System.out.println("Student id and Name");
         System.out.println(stud_id);
         System.out.println(stud_name);
     }
-    
-    void createProffesor(int member_cnt)
+   
+    void createProfessor(int member_cnt)
     {
-        
+       
         for(int i=0;i<member_cnt;i++)
         {
-            prof_id.add(i);
-            System.out.println("Enter Professor Name:");
+        	System.out.println("Enter name of Professor "+(i+1)+" : ");
             prof_name.add(sc.next());
+            prof_id.add(i+1);
+           
         }
-        System.out.println("Professor id and Name");
         System.out.println(prof_id);
         System.out.println(prof_name);    
     }
     
-    void CreateCourse()
+    void createCourse(int crs_cnt) 
     {
-    	crs_id.add(0);
-		crs_name.add("java");
-		crs_id.add(1);
-		crs_name.add("Sql");
-		crs_id.add(2);
-		crs_name.add("C++");
-		crs_id.add(3);
-		crs_name.add("Python");
-		crs_id.add(4);
-		crs_name.add("Devops");
+    	for(int i = 0;i<crs_cnt;i++) {
+    		System.out.println("Enter name of the course "+(i+1)+" : ");
+    		crs_name.add(sc.next());
+    		crs_id.add(i+1);
+    	}
+    	System.out.println(crs_id);
+    	System.out.println(crs_name);
     }
+
 }
 
 public class Admin {
     public static void main(String[] args) {
     	Scanner sc = new Scanner(System.in);
         AdminComponent ad = new AdminComponent();
-        ad.AdminLogin();
-        //addition of professor and student 
-        System.out.println("Enter the no. of Proffessor and Students to be inserted");
-        int member_cnt = sc.nextInt();
-        ad.createProffesor(member_cnt);
-        ad.createStudent(member_cnt);
-        
-        System.out.println("prof and students inserted successfully");
-        
-        Professor pf = new Professor();
-        pf.ProfessorDetails(ad.prof_id, ad.prof_name);
-        
-        Student st = new Student();
-        st.StudentDetails(ad.stud_id, ad.stud_name);
-        
-        System.out.println("couse id and Name");
-        ad.CreateCourse();
-        System.out.println(ad.crs_id);
-        System.out.println(ad.crs_name);
+        if(ad.AdminLogin()==true) {
+            System.out.println("Enter the no. of Professor and Students to be inserted : ");
+            int member_cnt = sc.nextInt();
+           
+            ad.createProfessor(member_cnt);
+            ad.createStudent(member_cnt);
+           
+            System.out.println("Professor and Students inserted successfully.");
+           
+            Professor pf = new Professor();
+            pf.ProfessorDetails(ad.prof_id, ad.prof_name);
+            Student st = new Student();
+            st.StudentDetails(ad.stud_id, ad.stud_name);
+        }
+        else {
+        	System.out.println("Try Again.");
+        }
+        sc.close();
     }
-    
+   
 }
+
